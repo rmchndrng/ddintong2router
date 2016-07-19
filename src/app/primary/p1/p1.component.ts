d708eb1c58942812345527767843ae48677277f6
Ramachandran G <g.rmchndrn@gmail.com>
2016-07-19 15:39:31 +0530

    Corrected the invalid route definition

--- src/app/primary/p1/p1.component.ts
+++ src/app/primary/p1/p1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import {Router, ActivatedRoute, UrlTree, UrlPathWithParams} from '@angular/router';
+import {UrlSegment} from '@angular/router/src/url_tree';
 @Component({
   moduleId: module.id,
   selector: 'app-p1',
@@ -11,8 +12,31 @@ export class P1Component implements OnInit {
   constructor(private router:Router,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit() {
-    console.log(this.router);
-    console.log(this.activatedRoute);
+    // console.log(this.router);
+    // console.log(this.activatedRoute);
+    // let currentUrlTree = this.router.parseUrl(this.router.url);
+    
+    // console.log(currentUrlTree);
+    // // let newUrlTree = this.router.createUrlTree(['right:r1']);
+    // // this.router.navigateByUrl(this.router.serializeUrl(newUrlTree));
+    // // let test:UrlPathWithParams =new UrlPathWithParams('r1',{});
+    // console.log(currentUrlTree.root.children["PRIMARY_OUTLET"]);
+    // let urlSegment:UrlSegment = currentUrlTree.root.children["PRIMARY_OUTLET"];
+    // let newAuxSegment:UrlSegment = new UrlSegment([new UrlPathWithParams('r1',{})],{});    
+    // let newAuxSegment2:UrlSegment = new UrlSegment([new UrlPathWithParams('l1',{})],{});
+    // let newUrlSegment:UrlSegment = new UrlSegment([],{
+    //   'PRIMARY_OUTLET':urlSegment,
+    //   'right':newAuxSegment,
+    //   'left':newAuxSegment2
+    // })
+    // console.log(newUrlSegment);
+    // let testUrlTree:UrlTree = new UrlTree();
+    // testUrlTree.root = newUrlSegment;
+    // testUrlTree.fragment= currentUrlTree.fragment;
+    // testUrlTree.queryParams = currentUrlTree.queryParams;
+    // this.router.navigateByUrl(this.router.serializeUrl(testUrlTree));
+    // // this.router.parseUrl()
+
   }
 
 }
--- src/app/primary/primary-routes.ts
+++ src/app/primary/primary-routes.ts
@@ -4,9 +4,9 @@ import {P2Component} from './p2'
 import {P3Component} from './p3'
 
 export const PrimaryRoutes: RouterConfig = [
-    { path: '', redirectTo: 'p1', pathMatch: 'full' },
-    { path: '**', redirectTo: 'p1'},
+    { path: '', redirectTo: 'p1', pathMatch: 'full' },    
     { path: 'p1', component:P1Component },
     { path: 'p2', component:P2Component },
-    { path: 'p3', component:P3Component }    
+    { path: 'p3', component:P3Component },
+    { path: '**', redirectTo: 'p1'},    
 ];
